25a43152cd492a2ac9d3f18cae6d1fb13736ddb1
Joseph Madsen <josephmads@protonmail.com>
2024-01-24 19:49:12 -0600

    delays functions for 3 secs after page load to allow needed elements to be present

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 The idea for this extension came from [Episode 141](https://darknetdiaries.com/episode/141/) of Darknet Diaries when, toward the end of the interview, the host Jack says:
 
 >"At a bare minimum, just show me a big, bright red banner on the e-mail that says, look out, this e-mail comes from a domain that was registered two days ago. That would be really cool."
+
+## Installation
+
+If you are interested in testing the `dbec` extension in its current form, you can follow these steps:
+
+1. Fork or clone this repo onto your local machine
+2. In Firefox: Open the [about:debugging](https://firefox-source-docs.mozilla.org/devtools-user/about_colon_debugging/index.html) page
+3. click the "This Firefox" option
+4. click the "Load Temporary Add-on" button
+5. then select any file in the `dbec` extension directory
+
+The extension now installs, and remains installed until you restart Firefox.
+
+## Try It Out
+
+1. Go to your Gmail account
+2. Open an email
--- dbec.js
+++ dbec.js
@@ -1,31 +1,84 @@
-const sender = document.body.getElementsByClassName("gD");
 
-sender[0].style.border = "3px solid red";
+function getSenderDomain() {
+  const sender = document.body.getElementsByClassName("gD");
 
-let senderEmail = sender[0].attributes.email.value;
-console.log(`This message is from ${senderEmail}`);
+  sender[0].style.border = "3px solid red";
+  
+  let senderEmail = sender[0].attributes.email.value;
+  console.log(`This message is from ${senderEmail}`);
+  
+  // use regex to isolate domain after @ and assign to variable
+  
+  const domainRegex = /(?<=@).*/g;
+  const domainArray = senderEmail.match(domainRegex);
+  const senderDomain = domainArray[0];
+  console.log(senderDomain);
+  
+  // Define the API URL
+  
+  const url = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
 
-// use regex to isolate domain after @ and assign to variable
+  return url;
+}
 
-const domainRegex = /(?<=@).*/g;
-const domainArray = senderEmail.match(domainRegex);
-const senderDomain = domainArray[0];
-console.log(senderDomain);
 
-// Define the API URL
-
-const url = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
 
 // Make a GET request
 
-async function getRdapRecords() {
-  return fetch(url).then(response => response.json());
-}
- 
-const rdapData  = await getRdapRecords();
 
-const registration = rdapData.events[0]["eventDate"]
+// Delay functions to allow necessary elements to load
 
+setTimeout(() => {
+  const apiURL = getSenderDomain();
+  console.log(apiURL)
+}, 3000);
+
+
+
+
+
+
+/* Not working fetches
+
+// async function getRdapRecords() {
+//   return fetch(url).then(response => response.json());
+// }
+
+// const rdapData  = await getRdapRecords();
+
+// const registration = rdapData.events[0]["eventDate"]
+
+
+
+// let rdapData;
+
+// fetch(url).then(
+//   function(u) { return u.json();}
+// ).then(
+//   function(json) {
+//     rdapData = json;
+//   }
+// )
+
+// const registration = rdapData.events[0]["eventDate"];
+// console.log(`${senderDomain} was registered on ${registration}`);
+
+
+
+// async function getRdapRecords() {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   const registration = await data.events[0]["eventDate"];
+//   console.log(data);
+// }
+
+// getRdapRecords()
+
+// const senderDomainReg = getRdapRecords();
+
+// const registration = rdapData.events[0]["eventDate"];
+
+// console.log(`${senderDomain} was registered on ${senderDomainReg}`);
 
 // fetch(url)
 //   .then((response) => {
@@ -35,9 +88,10 @@ const registration = rdapData.events[0]["eventDate"]
 //     return response.json();
 //   })
 //   .then((data) => {
-//     rdap = data;
+//     console.log(data);
 //   })
 //   .catch((error) => {
 //     console.error("Error:", error);
 //   });
-    
+
+*/
