b19f74959513e48d2ca7ae996d5476b6b82e9ea4
Joseph Madsen <josephmads@protonmail.com>
2024-01-26 20:35:53 -0600

    fix: permissions added, functions work

--- dbec.js
+++ dbec.js
@@ -1,46 +1,34 @@
 // dbec
 
-function getSenderDomain() {
+function main() {
   const sender = document.body.getElementsByClassName("gD");
   sender[0].style.border = "3px solid red"; //DELETE ME
   const senderEmail = sender[0].attributes.email.value;
-  console.log(`This message is from ${senderEmail}`); //DELETE ME
   
   // use regex to isolate domain after @ and assign to variable
-  
   const domainRegex = /(?<=@).*/g;
   const domainArray = senderEmail.match(domainRegex);
   const senderDomain = domainArray[0];
-  console.log(senderDomain); //DELETE ME
   
   // Define the API URL
+  const rdapUrl = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
   
-  const url = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
-  return url;
-}
+  // Make Get request to RDAP API
+  async function getRdapRecords(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const registration = data.events[0]["eventDate"]
+      console.log(`${senderDomain} was registered on ${registration}`)
+    } catch (error) {
+      console.error("Error:", error);
+    } 
+  }
 
-// Make a GET request
-
-async function getRdapRecords(url) {
-  try {
-    console.log(typeof(url), url) //DELETE ME
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data) 
-  } catch (error) {
-    console.error("Error:", error);
-  } 
-}
-
-function main() {
-  const apiURL = getSenderDomain();
-  console.log(apiURL)
-  getRdapRecords(apiURL)
+  getRdapRecords(rdapUrl)
 }
 
 // Delay functions to allow necessary elements to load
-
-
 setTimeout(() => {
   main();
 }, 3000);
