81a6b95b1d0419a1d9c47a7d4cb8ce50b58afc66
Joseph Madsen <josephmads@protonmail.com>
2024-02-03 21:45:25 -0600

    working, but not conistent

--- content_scripts/dbec.js
+++ content_scripts/dbec.js
@@ -1,20 +1,43 @@
 // dbec
 
+const userAlertStyle = `
+  background: #ff0000;
+  padding: .75rem;
+  border-radius: 5px;
+`;
+
 function main() {
   const sender = document.body.querySelector("span[email]");
   const senderEmail = sender.attributes.email.value;
-  
+
   // use regex to isolate domain after @ and assign to variable
   const domainRegex = /(?<=@).*/g;
   const domainArray = senderEmail.match(domainRegex);
   const senderDomain = domainArray[0];
-  
+  console.log("main1");
+
   // Define the API URL
-  const rdapUrl = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
-  
+  // const rdapUrl = `https://rdap.verisign.com/com/v1/domain/${senderDomain}`;
+
   // Make Get request to RDAP API
-  async function getRdapRecords(url) {
+  async function getRdapRecords(domain) {
     try {
+      const tld = domain.substr(domain.length - 3);
+      let url = "";
+      
+      // keep trying rdap.net/domain/
+
+      // replace with a for loop? 
+      if (tld === "com") {
+        url = `https://rdap.verisign.com/com/v1/domain/${domain}`;
+      } else if (tld === "net") {
+        url = `https://rdap.verisign.com/net/v1/domain/${domain}`;
+      } else {
+        url = `https://rdap.verisign.com/$TLD/v1/domain/${domain}`;
+        url = `https://rdap.donuts.co/rdap/domain/${domain}`;
+        url = `https://rdap.identitydigital.services/rdap/domain/${domain}`;
+      }
+
       const response = await fetch(url);
       const data = await response.json();
       const registration = data.events[0]["eventDate"];
@@ -23,17 +46,19 @@ function main() {
       // display message to user
       const target = document.getElementById(":1v");
       const userAlert = document.createElement("div");
-      userAlert.textContent = `${senderDomain} was registered on ${registration}`;
-      target.before(userAlert); 
+      userAlert.textContent = `${domain} was registered on ${registration}`;
+      userAlert.style.cssText = userAlertStyle;
+      target.before(userAlert);
     } catch (error) {
-      console.error("Error:", error);
-    } 
+      console.log("Error:", error);
+    }
   }
 
-  getRdapRecords(rdapUrl)
+  getRdapRecords(senderDomain);
 }
 
-// Delay functions to allow necessary elements to load
+// Delay functions to allow necessary elements to load -- needs to change to a eventlistener
 setTimeout(() => {
+  console.log("timeout");
   main();
-}, 3000);
+}, 5000);
