de3a511fa6f520f4e6e00b2736a24049ba331aed
Joseph Madsen <josephmads@protonmail.com>
2024-03-25 09:51:35 -0500

    update

--- content_scripts/dbec.js
+++ content_scripts/dbec.js
@@ -3,7 +3,7 @@
 const userAlertStyle = `
   background: #ff8080;
   padding: .75rem;
-  max-width: 30rem;
+  max-width: 35rem;
   border-radius: 5px;
 `;
 
@@ -11,34 +11,12 @@ function main() {
   const getSender = document.body.querySelector("span[email]");
   const senderEmail = getSender.attributes.email.value;
 
-  /**
-   * Isolates the domain from the email address
-   * @param {string} email
-   * @returns {string} isolated domain
-   */
-  function isolateDomain(email) {
-    const domainRegex = /(?<=@).*/gi;
-    const domainArray = email.match(domainRegex);
-    const domain = domainArray[0];
-
-    const checkForSubDomain = (domain.match(/\./g)||[]).length;
-
-    if (checkForSubDomain > 1) {
-      senderArray = domain.split('.');
-      let tld = senderArray.pop()
-      let sld = senderArray.pop()
-      const cleanedDomain = `${sld}.${tld}`
-      return cleanedDomain
-    } else {
-      return domain
-    }
-  }
-
-  const senderDomain = isolateDomain(senderEmail)
+  const regex = /(?<=@)(?:.*\.)?([^\.]+\.[^\.]+)/;
+  const domainArray = senderEmail.match(regex);
+  const senderDomain = domainArray[1];
 
   /**
-   * Retrieves domain records from RDAP 
-   * and displays registration date to user
+   * Retrieves domain records from RDAP; displays registration date to user
    * @param {string} domain 
    */
   async function getRdapRecords(domain) {
@@ -59,6 +37,7 @@ function main() {
     }
   }
 
+  // const senderDomain = isolateDomain(senderEmail);
   getRdapRecords(senderDomain);
 }
 
@@ -69,9 +48,16 @@ const urlFragArray = urlFrag.match(urlRegex);
 const urlFragMatch = urlFragArray[0];
 
 // Delay functions to allow necessary elements to load -- needs to change to a eventlistener
-setTimeout(() => {
-  console.log("timeout");
+// setTimeout(() => {
+//   console.log("timeout");
+//   if (window.location.hash === urlFragMatch) {
+//     main();
+//   }
+// }, 5000);
+
+window.navigation.addEventListener("navigate", (event) => {
+  console.log('nav')
   if (window.location.hash === urlFragMatch) {
     main();
   }
-}, 5000);
+})
